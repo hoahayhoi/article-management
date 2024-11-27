41ab41dbe8bb699633113cdb84cc202944111c2d
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-27 15:51:31 +0700

    Setup project graphql api

---
+++ index.ts
@@ -0,0 +1,35 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connect } from "./config/database";
+connect();
+
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
+
+const startServer = async () => {
+    const app: any = express();
+    const port: number = 3000;
+
+    // GraphQL API
+    const apolloServer = new ApolloServer({
+        typeDefs: typeDefs,
+        resolvers: resolvers
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({
+        app: app,
+        path: "/graphql"
+    });
+
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`);
+    });
+}
+
+
+startServer();
